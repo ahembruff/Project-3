dff7d0979e73116fccbed1fd059739f26d3fb0a4
ahembruff <168450462+ahembruff@users.noreply.github.com>
2024-11-17 13:13:35 -0500

    Update HembruffAidan_Project3.py
    Commenting, returning dimensionality to R and M and plotting. Still troubleshooting solve_ivp

--- HembruffAidan_Project3.py
+++ HembruffAidan_Project3.py
@@ -12,27 +12,47 @@ import matplotlib.pyplot as plt
 
 # Part 1
 
+# function to pass to solve_ivp
 def ODE(r, ystate):
-    rho = ystate[0]
-    m = ystate[1]
+    rho = ystate[0]  # dimensionless density
+    m = ystate[1]    # dimensionless mass
     
-    x = rho**(1/3)
-    gamma = (x**2)/(3*(np.sqrt(1+(x**2))))
+    x = rho**(1/3)   # defining x to be used in gamma function
+    gamma = (x**2)/(3*(np.sqrt(1+(x**2)))) # gamma function for derivative of rho
     
-    drho = -(m*rho)/(gamma*(r**2))
-    dm = (r**2)*rho
+    drho = -(m*rho)/(gamma*(r**2)) # derivative of rho
+    dm = (r**2)*rho                # derivative of mass
     
-    dystate = [drho,dm]
+    dystate = [drho,dm]            # state vector derivative
     
     return dystate
 
-rho_c = np.linspace(1e-1,2.5e6,10)
-r_outer = np.sqrt((6*np.sqrt((rho_c**(2/3))+1))/(rho_c**(1/3)))
+# initializing the range of rho_c values and the outer limit of r for which rho = 0
+rho_c = np.array((1e-1,10,100,1e3,1e4,5e4,1e5,5e5,1e6,2.5e6))
+r_outer = np.sqrt((6.*np.sqrt((rho_c**(2/3))+1.))/(rho_c**(1/3)))
+
+# desired number of nucleons per electron for this problem
+mu_e = 2.
+# constants which remove dimensionality from r and m, reintroduced to return
+# dimensionality for plotting when solution is found
+R_0 = (7.72e8)/mu_e
+M_0 = (5.67e33)/(mu_e**2)
 
 soln_list = []
+# loop over all values of rho_c
 for i in range(len(rho_c)):
-    boundary_conds = [rho_c[i],0]
-    soln = solve_ivp(ODE,(1e-6,r_outer[i]),boundary_conds,method="RK45")
-    soln_list.append(soln)
-    plt.plot(soln.t,soln.y[0])
-    plt.show()
+    r_eval = np.linspace(1e-6,r_outer[i],500)
+    boundary_conds = [rho_c[i],0] # boundary conditions for each instance of rho_c
+    soln = solve_ivp(ODE,(1e-6,r_outer[i]),boundary_conds,method="RK45",t_eval = r_eval) # solve_ivp using RK45 method
+    
+    dimensional_r = soln.t*R_0
+    dimensional_m = soln.y[1]*M_0
+
+    plt.plot(dimensional_m,dimensional_r, label = rho_c[i])
+    
+plt.title("Radius as a function of Mass for White Dwarf Stars")
+plt.xlabel("Mass (M)")
+plt.ylabel("Radius (R)")
+plt.legend()
+
+plt.show()
