6113620c67ac5a42264332f4c7ab9517a774f017
ahembruff <168450462+ahembruff@users.noreply.github.com>
2024-11-20 16:09:21 -0500

    Update HembruffAidan_Project3.py
    Modifying code to terminate integration at the event of rho = 0 , and plot the final mass and radius

--- HembruffAidan_Project3.py
+++ HembruffAidan_Project3.py
@@ -17,32 +17,28 @@ def ODE(r, ystate):
     rho = ystate[0]  # dimensionless density
     m = ystate[1]    # dimensionless mass
     
-    x = rho**(1/3)   # defining x to be used in gamma function
+    x = np.cbrt(rho)   # defining x to be used in gamma function
     gamma = (x**2)/(3*(np.sqrt(1+(x**2)))) # gamma function for derivative of rho
     
-    drho = -(m*rho)/(gamma*(r**2)) # derivative of rho
+    drho = (-m*rho)/(gamma*(r**2)) # derivative of rho
     dm = (r**2)*rho                # derivative of mass
     
     dystate = [drho,dm]            # state vector derivative
     
     return dystate
 
+# event function to find when mass and density are zero
 def event(r,ystate):
-    rho = ystate[0]  # dimensionless density
-    m = ystate[1]    # dimensionless mass
-    
-    x = rho**(1/3)   # defining x to be used in gamma function
-    gamma = (x**2)/(3*(np.sqrt(1+(x**2)))) # gamma function for derivative of rho
-    
-    drho = -(m*rho)/(gamma*(r**2)) # derivative of rho
-    dm = (r**2)*rho                # derivative of mass
-    
-    dystate = [drho,dm]            # state vector derivative
-    
-    return rho
+    if ystate[0] - 1e-5 < 0:
+        ystate[0] = 0
+    return ystate[0] 
+
+# stop integration when rho equals 0
+event.terminal = True
+event.direction= -1
+
 # initializing the range of rho_c values and the outer limit of r for which rho = 0
-rho_c = np.array((1e-1,10,100,1e3,1e4,5e4,1e5,5e5,1e6,2.5e6))
-r_outer = np.sqrt((6.*np.sqrt((rho_c**(2/3))+1.))/(rho_c**(1/3)))
+rho_c = np.logspace(-1,6.3,10)
 
 # desired number of nucleons per electron for this problem
 mu_e = 2.
@@ -54,14 +50,15 @@ M_0 = (5.67e33)/(mu_e**2)
 soln_list = []
 # loop over all values of rho_c
 for i in range(len(rho_c)):
-    r_eval = np.linspace(1e-6,0.5,500)
+    r_eval = np.linspace(1e-6,10,200)
     boundary_conds = [rho_c[i],0] # boundary conditions for each instance of rho_c
-    soln = solve_ivp(ODE,(1e-6,0.5),boundary_conds,method="RK45",t_eval = r_eval, events = event) # solve_ivp using RK45 method
+    # solve_ivp using RK45 method
+    soln = solve_ivp(ODE,(1e-6,10),boundary_conds,method="RK45",t_eval = r_eval, events = event) 
     
-    dimensional_r = soln.t*R_0
-    dimensional_m = soln.y[1]*M_0
+    dimensional_r = soln.t_events[0]*R_0
+    dimensional_m = soln.y_events[0][0][1]*M_0
 
-    plt.plot(dimensional_m,dimensional_r, label = rho_c[i])
+    plt.scatter(dimensional_m,dimensional_r, label = rho_c[i])
     
 plt.title("Radius as a function of Mass for White Dwarf Stars")
 plt.xlabel("Mass (M)")
